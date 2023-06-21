3209ac6018da08c89adbcb99ac542cfc18d47f26
Osema Fadhel <ofadhel@e3r6p2.42roma.it>
2023-06-21 23:59:46 +0200

    push_swap

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 10:25:12 by ofadhel           #+#    #+#             */
-/*   Updated: 2023/06/21 19:18:18 by ofadhel          ###   ########.fr       */
+/*   Updated: 2023/06/21 23:57:08 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,9 @@ int	main(int ac, char **av)
 		size = ft_add_nb(&stack_a, av);
 	check_doubles(&stack_a);
 	if (!check_sorted(&stack_a))
+	{
 		algorithm(&stack_a, &stack_b, size);
-	if (!check_sorted(&stack_a))
-		ft_printf("KO\n");
-	free_stack(stack_a);
+		free_stack(stack_a);
+	}
 	return (0);
 }
